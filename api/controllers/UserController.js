a43b53ffdcd29d041c659ad780c32e3fd9b15151
haadi_i <haadi_i@haadi.localdomain>
2020-09-05 15:29:04 -0400

    feat(authentication): add little sanity check on incoming request's body

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,8 +8,24 @@
 let baseUrl = 'https://staging.api.external.thegoodseat.fr/';
 let request = require('request');
 
+function checkBody(body, isLogin) {
+  let mandatorySignupKeys = ['firstName', 'lastName', 'email', 'birthDate', 'phoneNumber', 'country', 'isPhoneNumberVerified'];
+  let mandatoryLoginKeys = ['email', 'phoneNumber'];
+  let k = true;
+  for (let key of (isLogin? mandatoryLoginKeys : mandatorySignupKeys)) {
+    if (!(key in body)) {
+      k = false;
+    }
+  }
+  return k;
+}
+
 module.exports = {
   register: async function(req, res) {
+    if (!checkBody(req.body, false)) {
+      res.status(400).send({message: 'Bad request.'});
+      return;
+    }
     let url = baseUrl + 'registeruser';
     request(url,{
       method: "POST",
@@ -37,6 +53,10 @@ module.exports = {
   },
 
   login: async function(req, res) {
+    if (!checkBody(req.body, true)) {
+      res.status(400).send({message: "Bad request."})
+      return;
+    }
     let url = baseUrl + 'loginuser';
     request(url,{
       method: "POST",
